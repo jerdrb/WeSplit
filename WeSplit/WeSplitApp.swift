1d522669b16f6001915f5ee341206a8b1b3671f2
jerdrb <jerdrb@gmail.com>
2022-06-10 11:09:20 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jared Bell on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
