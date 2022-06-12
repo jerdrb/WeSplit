209291495118a84fe3aa03816d6bcbd7e0d5e11c
jerdrb <jerdrb@gmail.com>
2022-06-12 13:32:58 -0500

    Challenge Updates
    Added additional challenge features and added property for currency format rather than calling it with each instance.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,26 +12,27 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
-    
+    var currencyFormatter: FloatingPointFormatStyle<Double>.Currency =
+        FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currencyCode ?? "USD")
     let tipPercentages = [10,15,20,25,0]
-    
-    var totalPerPerson: Double {
-        //calculating total here
-        let peopleCount = Double(numberOfPeople + 2)
+    var grandTotal: Double {
         let tipSelection = Double(tipPercentage)
         
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
     }
-    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    TextField("Amount", value: $checkAmount, format: currencyFormatter)
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     
@@ -44,18 +45,25 @@ struct ContentView: View {
                 
                 Section {
                     Picker("Tip Percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
+                
+                Section {
+                    Text(grandTotal, format: currencyFormatter)
+                } header: {
+                    Text("Grand Total")
+                }
             
         
-            Section {
-                Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                Section {
+                    Text(totalPerPerson, format: currencyFormatter)
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationTitle("WeSplit")
